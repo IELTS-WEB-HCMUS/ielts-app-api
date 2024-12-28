c8b3f5faa66e183a6c029319983e52fd89df903f
Nguyen Trong Nhan <info.nhan925@gmail.com>
2024-12-28 14:58:17 +0700

    update look up vocab with ai api

--- internal/models/ai.go
+++ internal/models/ai.go
@@ -5,15 +5,17 @@ import (
 )
 
 type Vocab struct {
-	ID            int    `json:"id" gorm:"column:id;primaryKey;autoIncrement"`
-	VocabID       string `json:"-" gorm:"column:vocab_id;not null"`
-	Value         string `json:"value" gorm:"column:value;not null"`
-	WordClass     string `json:"word_class" gorm:"column:word_class;not null"`
-	Meaning       string `json:"meaning" gorm:"column:meaning;not null"`
-	IPA           string `json:"ipa" gorm:"column:ipa;not null"`
-	Explanation   string `json:"explanation" gorm:"column:explanation;not null"`
-	Collocation   string `json:"collocation" gorm:"column:collocation;not null"`
-	VerbStructure string `json:"verb_structure" gorm:"column:verb_structure;not null"`
+	ID          int    `json:"id" gorm:"column:id;primaryKey;autoIncrement"`
+	VocabID     string `json:"-" gorm:"column:vocab_id;not null"`
+	Value       string `json:"-" gorm:"column:value;not null"`
+	WordDisplay string `json:"word_display" gorm:"column:word_display;not null"`
+	WordClass   string `json:"word_class" gorm:"column:word_class;not null"`
+	Meaning     string `json:"meaning" gorm:"column:meaning;not null"`
+	IPA         string `json:"ipa" gorm:"column:ipa;not null"`
+	Explanation string `json:"explanation" gorm:"column:explanation;not null"`
+	Collocation string `json:"collocation" gorm:"column:collocation;not null"`
+	Example     string `json:"example" gorm:"column:example;not null"`
+	VoteCount   int    `json:"-" gorm:"column:vote_count;not null"`
 }
 
 // TableName overrides the default table name for GORM
@@ -22,6 +24,8 @@ func (Vocab) TableName() string {
 }
 
 type LookUpVocabRequest struct {
-	QuizId     int `json:"quiz_id" binding:"required"`
-	VocabIndex int `json:"vocab_index" binding:"required"`
+	QuizId        int    `json:"quiz_id" binding:"required"`
+	SentenceIndex int    `json:"sentence_index" binding:"required"`
+	WordIndex     int    `json:"vocab_index" binding:"required"`
+	Word          string `json:"word" binding:"required"`
 }
--- internal/services/ai.go
+++ internal/services/ai.go
@@ -6,17 +6,46 @@ import (
 	"ielts-web-api/common"
 	"ielts-web-api/internal/models"
 
+	"errors"
+
 	"gorm.io/gorm"
 )
 
+func (s *Service) LookUpVocabLinear(ctx context.Context, quizId int, sentenceIndex int, word string) (*models.Vocab, error) {
+	vocabIdPattern := fmt.Sprintf("%d_%d_%%", quizId, sentenceIndex)
+
+	vocab, err := s.vocabRepo.GetDetailByConditions(ctx, func(tx *gorm.DB) {
+		tx.Where("vocab_id LIKE ? AND value = ?", vocabIdPattern, word)
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return vocab, nil
+}
+
 func (s *Service) LookUpVocab(ctx context.Context, req models.LookUpVocabRequest, userId string) (*models.Vocab, error) {
-	vocabId := fmt.Sprintf("%d_%d", req.QuizId, req.VocabIndex)
+	vocabId := fmt.Sprintf("%d_%d_%d", req.QuizId, req.SentenceIndex, req.WordIndex)
 
 	vocab, err := s.vocabRepo.GetDetailByConditions(ctx, func(tx *gorm.DB) {
 		tx.Where("vocab_id = ?", vocabId)
 	})
 	if err != nil {
-		return nil, err
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			vocab, err = s.LookUpVocabLinear(ctx, req.QuizId, req.SentenceIndex, req.Word)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			return nil, err
+		}
+	} else {
+		if vocab.Value != req.Word {
+			vocab, err = s.LookUpVocabLinear(ctx, req.QuizId, req.SentenceIndex, req.Word)
+			if err != nil {
+				return nil, err
+			}
+		}
 	}
 
 	user, err := s.userRepo.GetByID(ctx, userId)
